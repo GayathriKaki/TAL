2ef20ee35517d481609a35971842ce8a7bc573ce
phani <phani@HOME-PGBY>
2021-08-07 13:34:45 +1000

    Add project files.

---
+++ TAL.DB/Insert-Queries.sql
@@ -0,0 +1,6 @@
+insert into Rating(RatingName,Factor) values('Professional',1.1),
+('White Collar',1.45),('Light Manual',1.70),('Heavy Manual',2.1);
+
+insert into Occupation(OccupationName,RatingId)values('Çleaner',3)
+,('Doctor',1),('Author',2),('Farmer',4),('Mechanic',4),('Florist',3)
+
